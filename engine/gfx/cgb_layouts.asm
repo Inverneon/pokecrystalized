bf6e1d4b238b98305e839b92710846f4f37a4e1a
Nayru62 <Nayru62crystal@gmail.com>
2022-09-17 20:07:46 -1000

    4th stats page
    fix 4th stats page
    
    had constants changes from a different commit

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -328,6 +328,11 @@ ENDC
 	ld a, $7 ; mon base type light/dark pals
 	call FillBoxCGB
 
+	hlcoord 17, 5, wAttrmap
+	lb bc, 2, 2
+	ld a, $6 ; orange page palette
+	call FillBoxCGB
+
 	call ApplyAttrmap
 	call ApplyPals
 	ld a, TRUE
