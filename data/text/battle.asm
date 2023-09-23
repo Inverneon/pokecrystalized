96bc6d68dea1c3824d6edd48c60020eaa9b4e6f1
Nayru62 <Nayru62crystal@gmail.com>
2023-09-23 18:57:07 -0400

    distinguish status move misses and fails
    https://github.com/pret/pokecrystal/wiki/Make-new-battle-text-to-distinguish-status-move-misses-and-fails

--- data/text/battle.asm
+++ data/text/battle.asm
@@ -689,6 +689,15 @@ AlreadyAsleepText:
 	line "already asleep!"
 	prompt
 
+AlreadyBurnedText:
+	text "<TARGET>'s"
+	line "already burned!"
+	prompt
+
+AlreadyFrozenText:
+	text "<TARGET>'s"
+	line "already frozen!"
+	prompt
 WasPoisonedText:
 	text "<TARGET>"
 	line "was poisoned!"
