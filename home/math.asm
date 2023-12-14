cfc8d1e7383aa9a11690f3af6c8df0be82cd587c
Nayru62 <Nayru62crystal@gmail.com>
2023-12-14 00:11:23 -0500

    Adjust_percent FIXED
    Thanks to ax6 and SonicRay!!
    Theirs:
    ; in: a: value (x/256)
    ; out: a: value (x/100)
    ; clobbers hl
        ld l, a
        ld h, 0
        push af
    ; multiply by 3...
        add hl, hl
        add a, l
        ld l, a
        adc h
        sub l
        ld h, a
    ; ...now by 8, giving 24...
        add hl, hl
        add hl, hl
        add hl, hl
    ; ...and add the original value back, giving 25
        pop af
        add a, l
        ld l, a
        adc h
        sbc l
        ld h, a
    ; then multiply by 4, since 25 x 4 = 100
        add hl, hl
        add hl, hl
    ; round the high byte to nearest, drop the low byte and done!
        ld l, 0.5 ; SonicRay added this line to fix rounding errors
        sla l
        sbc a
        and 1
        add a, h
        ret
    
    Fixed Adjust Percent FOR REAL
    
    was using Multiply wrong, how embarassing

--- home/math.asm
+++ home/math.asm
@@ -25,7 +25,7 @@ SimpleDivide::
 	ret
 
 Multiply::
-; Multiply hMultiplicand (3 bytes) by hMultiplier. Result in hProduct.
+; Multiply hMultiplicand (3 bytes) by hMultiplier (1 byte) Result in hProduct (4 bytes).
 ; All values are big endian.
 	push hl
 	push bc
@@ -58,22 +58,32 @@ SubtractAbsolute:: ; unreferenced
 	ret
 
 Adjust_percent::
-	; hMultiplicand 
-	; hMultiplier. Result in hProduct.
-	ldh [hMultiplicand], a
-	ld a, 100
-	ldh [hMultiplier], a
-	call Multiply
-	; Divide hDividend length b (max 4 bytes) by hDivisor. Result in hQuotient.
+; in: a: value (x/256)
+; out: a: value (x/100)
+	; Multiply hMultiplicand (3 bytes) by hMultiplier (1 byte) Result in hProduct (4 bytes).
 	; All values are big endian.
-	ld b, 2
-	; ldh a, [hProduct]
-	; ldh [hDividend], a
-	ld a, 255
-	ldh [hDivisor], a
-	call Divide
-	ldh a, [hQuotient + 3]
-	cp 100
-	ret z
-	inc a
-	ret
+; cross multiply by 100, then divide by 256	
+	
+	; hMultiplicand (3 bytes) is big endian, so least significant goes to the right	
+	ldh [hMultiplicand + 2], a ; 3rd byte of hMultiplicand
+	; our value is out of 256 (but still only a max of 255 so 1 byte)
+	; so we are always only going to need the 3rd byte of hMultiplicand 
+	
+	; zero out the other two bytes
+	xor a
+	ldh [hMultiplicand + 1], a
+	ldh [hMultiplicand], a
+	
+	ld a, 100
+	ldh [hMultiplier], a ; 1 byte only
+	call Multiply
+
+	; since we know 255 is the max possible value, that means our Product will NEVER exceed 2-bytes
+	; because we are cross multiplying and now need to divide by 256
+	ldh a, [hProduct + 2] ; high byte of the 2-byte Product of the cross multiplication, low byte is hProduct + 3
+	; hProduct + 0 and hProduct + 1 will never have anything but zero since we'll never exceed a 2-byte Product
+	
+    and a ; check if our result is zero
+    ret z ; if zero, done
+    inc a ; else, add one
+    ret
